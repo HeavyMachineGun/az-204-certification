34489cc7c60a02ef92f9273b478a8e42fda1d686
Nestor Antonio Mejia Perez <NestorAntonio.MejiaPerez@thomsonreuters.com>
2022-10-12 07:32:13 -0500

    redis cache

---
+++ az204-redis-cache/RedisCache/Program.cs
@@ -0,0 +1,29 @@
+﻿using StackExchange.Redis;
+using System.Threading.Tasks;
+
+public class Program{
+
+    static string connectionString = "az204redis18678.redis.cache.windows.net:6380,password=IZTZgxsaaYBt8M7h7OAyQciwW80PLJ1neAzCaAbrqYU=,ssl=True,abortConnect=False";
+
+    public static async Task Main(string[] args){
+
+        using(var cache = ConnectionMultiplexer.Connect(connectionString)){
+            IDatabase db = cache.GetDatabase();
+
+            var result = await db.ExecuteAsync("ping");
+            Console.WriteLine($"PING =  {result.Type} : {result}");
+
+            bool setValue = await db.StringSetAsync("test:key",100);
+            Console.WriteLine($"SET =  {setValue}");
+
+
+            var getValue = await db.StringGetAsync("test:key");
+            Console.WriteLine($"GET =  {getValue}");
+
+            
+
+        }
+
+    }
+
+}
